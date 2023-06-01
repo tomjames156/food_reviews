f11139dd2cc70301236d1d44239854ff37b6fb49
Akinwande Tomisin <tomjames156@gmail.com>
2023-06-01 18:25:45 +0100

    add a side navigation

--- about.js
+++ about.js
@@ -1,6 +1,28 @@
 const imageContainer = document.getElementById('my-image-container');
 let myImage = imageContainer.children[0];
-let imageLocation = imageContainer.children[1]
+let imageLocation = imageContainer.children[1];
+const sideNavBtn = document.getElementById('mobile-nav-btn');
+const closeSideNavBtn = document.getElementById('close-side-nav');
+const sideNav = document.getElementById('side-nav');
+const mainContent = document.querySelector('main');
+const mainNav = document.querySelector('nav#nav');
+
+
+sideNavBtn.addEventListener('click', () => {
+    sideNav.classList.remove('hide');
+    mainContent.style.filter = `opacity(0.5)`;
+    mainContent.style.pointerEvents = 'none';
+    mainNav.style.filter = `opacity(0.9)`;
+    mainNav.style.pointerEvents = 'none';
+})
+
+closeSideNavBtn.addEventListener('click', () => {
+    sideNav.classList.add('hide');
+    mainContent.style.filter = `opacity(1)`;
+    mainContent.style.pointerEvents = 'auto';
+    mainNav.style.filter = `opacity(1)`;
+    mainNav.style.pointerEvents = 'auto';
+})
 
 const my_pictures = [ 
     {image: 'tomi_black.jpeg', alt_text: 'Tomi wearing black standing next to a car', location: 'Christmas Village Calabar'}, 
--- blog.js
+++ blog.js
@@ -6,6 +6,28 @@ let reviewRating = document.getElementById('rating');
 let reviewText = document.getElementById('full_review_text');
 let reviewList = document.getElementById('review_list');
 const restaurants = document.querySelectorAll('.restaurant');
+const sideNavBtn = document.getElementById('mobile-nav-btn');
+const closeSideNavBtn = document.getElementById('close-side-nav');
+const sideNav = document.getElementById('side-nav');
+const mainContent = document.querySelector('main');
+const mainNav = document.querySelector('nav#nav');
+
+
+sideNavBtn.addEventListener('click', () => {
+    sideNav.classList.remove('hide');
+    mainContent.style.filter = `opacity(0.5)`;
+    mainContent.style.pointerEvents = 'none';
+    mainNav.style.filter = `opacity(0.9)`;
+    mainNav.style.pointerEvents = 'none';
+})
+
+closeSideNavBtn.addEventListener('click', () => {
+    sideNav.classList.add('hide');
+    mainContent.style.filter = `opacity(1)`;
+    mainContent.style.pointerEvents = 'auto';
+    mainNav.style.filter = `opacity(1)`;
+    mainNav.style.pointerEvents = 'auto';
+})
 
 for (let restaurant of restaurants){
     restaurant.addEventListener('mouseover', () => {
