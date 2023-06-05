049b0f507f4e36a065643eaa36c68a8972de67af
Akinwande Tomisin <tomjames156@gmail.com>
2023-06-05 15:55:08 +0100

    updated the contact page and nav links

--- contact.js
+++ contact.js
@@ -3,6 +3,13 @@ const closeSideNavBtn = document.getElementById('close-side-nav');
 const sideNav = document.getElementById('side-nav');
 const mainContent = document.querySelector('main');
 const mainNav = document.querySelector('nav#nav');
+const submitBtn = document.getElementById("submit_btn");
+let submitLink = document.getElementById('send_btn');
+let fullNameInput = document.getElementById('full_name').value;
+let emailInput = document.getElementById('email').value;
+let subjectInput = document.getElementById('message_subject').value;
+let messageBodyInput = document.getElementById('message').value;
+const emailForm = document.getElementById('email_form');
 
 
 sideNavBtn.addEventListener('click', () => {
@@ -20,3 +27,24 @@ closeSideNavBtn.addEventListener('click', () => {
     mainNav.style.filter = `opacity(1)`;
     mainNav.style.pointerEvents = 'auto';
 })
+
+// submitLink.addEventListener('click', () => {
+//     emailForm.submit();
+//     submitLink.click();
+// })
+
+// emailForm.addEventListener('submit', () => {
+//     submitLink.setAttribute("href", `mailto:tomjgjhgkhjos156@gmail.com?subject=Yeahhh&body=Yeahhh`);
+// })
+
+
+function mimeText(sentence){
+    let words = sentence.split(' ');
+    let mimeText = words[0];
+
+    for(i = 1; i < words.length; i++){
+        mimeText += `%20${words[i]}`
+    }
+
+    return mimeText;
+};
